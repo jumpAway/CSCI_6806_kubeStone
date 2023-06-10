4556a9a5ce4e3200edded58b8cd4e5496cb38c39
jumpAway <guohaolica@163.com>
2023-06-10 10:33:18 -0700

    basic_httpframework

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"kubeStone/m/v2/pkg/server"
+	"log"
+)
 
 func main() {
-	fmt.Println("hello")
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
