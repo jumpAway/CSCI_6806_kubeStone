eb3a5153e0b35de385e8611fae98993e297ae138
jerryruan128 <jerryruan128@gmail.com>
2023-06-21 17:43:05 -0700

    Add some comment to config section

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -6,11 +6,17 @@ import (
 	"os"
 )
 
+/*
+The Config struct represents a configuration object.
+It is used to hold configurations for a software application.
+*/
 type Config struct {
 	Name     string `json:"name"`
 	Version  string `json:"version"`
 	Database struct {
-		Host         string `json:"host"`
+		//The hostname or IP address of the database server
+		Host string `json:"host"`
+		//The port number on which the database server is listening
 		Port         int    `json:"port"`
 		Username     string `json:"username"`
 		Password     string `json:"password"`
@@ -18,9 +24,12 @@ type Config struct {
 		Volume       string `json:"volume"`
 	} `json:"database"`
 	Server struct {
+		//The port number on which the application server should listen
 		Port int `json:"port"`
 	} `json:"server"`
 }
+
+// The Server struct encapsulates information about a server.
 type Server struct {
 	Hostname string `json:"hostname"`
 	IP       string `json:"ipaddress"`
@@ -29,6 +38,8 @@ type Server struct {
 	Password string `json:"password"`
 }
 
+/* InitConfig is a function that reads a configuration file named "config.json",
+deserializes the JSON data into a Config struct, and returns it.*/
 func InitConfig() (config Config, err error) {
 	data, err := os.ReadFile("config.json")
 	if err != nil {
